4f93c3f1fc870b275d96d3bf03e079e0f8e0c126
enibigir <emery.nibigira@cern.ch>
2021-04-01 09:46:59 +0200

    fresh start

---
+++ Analyzer/plugins/Analyzer.cc
@@ -0,0 +1,144 @@
+// -*- C++ -*-
+//
+// Package:    SUSYBSMAnalysis/Analyzer
+// Class:      Analyzer
+//
+/**\class Analyzer Analyzer.cc SUSYBSMAnalysis/Analyzer/plugins/Analyzer.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Emery Nibigira
+//         Created:  Thu, 01 Apr 2021 07:04:53 GMT
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+ #include "FWCore/Utilities/interface/InputTag.h"
+ #include "DataFormats/TrackReco/interface/Track.h"
+ #include "DataFormats/TrackReco/interface/TrackFwd.h"
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<>
+// This will improve performance in multithreaded jobs.
+
+
+using reco::TrackCollection;
+
+class Analyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+   public:
+      explicit Analyzer(const edm::ParameterSet&);
+      ~Analyzer();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+
+      // ----------member data ---------------------------
+      edm::EDGetTokenT<TrackCollection> tracksToken_;  //used to select what tracks to read from configuration file
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+Analyzer::Analyzer(const edm::ParameterSet& iConfig)
+ :
+  tracksToken_(consumes<TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tracks")))
+
+{
+   //now do what ever initialization is needed
+
+}
+
+
+Analyzer::~Analyzer()
+{
+
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+Analyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+
+   for(const auto& track : iEvent.get(tracksToken_) ) {
+      // do something with track parameters, e.g, plot the charge.
+      // int charge = track.charge();
+   }
+
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+   ESHandle<SetupData> pSetup;
+   iSetup.get<SetupRecord>().get(pSetup);
+#endif
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void
+Analyzer::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void
+Analyzer::endJob()
+{
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+Analyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+
+  //Specify that only 'tracks' is allowed
+  //To use, remove the default given above and uncomment below
+  //ParameterSetDescription desc;
+  //desc.addUntracked<edm::InputTag>("tracks","ctfWithMaterialTracks");
+  //descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(Analyzer);
--- README.md
+++ README.md
@@ -3,43 +3,53 @@
 ## Setup working area
 
 ```bash
-export SCRAM_ARCH=slc6_amd64_gcc630
-
-cmsrel CMSSW_9_4_3
-cd CMSSW_9_4_3/src/
+export SCRAM_ARCH=slc7_amd64_gcc700
+cmsrel CMSSW_10_6_20
+cd CMSSW_10_6_20/src/
 cmsenv
+```
 
-git cms-init
-# for the following step you should have a GitHub's ssh key
-git clone git@github.com:CMS-HSCP/SUSYBSMAnalysis-HSCP.git SUSYBSMAnalysis/HSCP 
+For the following step you should have a ssh key associated to your GitLab account
 
+```bash
+git clone -b dev git@github.com:enibigir/SUSYBSMAnalysis-HSCP.git SUSYBSMAnalysis 
+```
+<!--
 # Before compile, hide BigNTuplizer 
 pushd SUSYBSMAnalysis/HSCP/plugins
 mv BigNtuplizer.cc BigNtuplizer.cc.bkp
 popd
+-->
 
+To compile the code, run
+```bash
+cd SUSYBSMAnalysis
 scram b -j8
 ```
 
--------------------------------
-
 ## Run the code
 
-Steps:
+Get the scripts first:
+```bash
+cp scripts/HSCParticleProducer_cfg.py scripts/crabConfig_Data.py .
+cp scripts/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt .
+```
 
-1. Make a first skim from the AOD to some lighter AOD with only the collections needed:
+Get proxy:
+```bash
+voms-proxy-init --voms cms -valid 192:00
+```
 
-	Directory : `/test/MakeEDMtuples`
-	Code: 
-	- HSCParticleProducerSingleMu2017_cfg.py and HSCParticleProducerMET2017_cfg.py:  configuration files to run on the SingleMuon and MET datasets, respectively
-	- crab_cfg.py crab configuration file
+A quick test:
+```bash
+#file exists?
+dasgoclient -query="site file=/store/data/Run2017B/MET/AOD/09Aug2019_UL2017_rsb-v1/00000/AA1FC1E6-1E88-204D-B867-4637AEAC4BEA.root"
+#if file exists on DISK (not on Tape), then do the following
+cmsRun HSCParticleProducer_cfg.py LUMITOPROCESS=Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt inputFiles=root://cms-xrd-global.cern.ch//store/data/Run2017B/MET/AOD/09Aug2019_UL2017_rsb-v1/00000/AA1FC1E6-1E88-204D-B867-4637AEAC4BEA.root
+```
 
-2. test/UsefulScripts/DeDxStudy. Go to the [README](./test/UsefulScripts/DeDxStudy/README.md)
-
-3. test/UsefulScripts/MuonTimingStudy
-
-4. ...
-
-
-
-
+Then run crab:
+```bash
+source /cvmfs/cms.cern.ch/common/crab-setup.sh
+python crabConfig_Data.py
+```
