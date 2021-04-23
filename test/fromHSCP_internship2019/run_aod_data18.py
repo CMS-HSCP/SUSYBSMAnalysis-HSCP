871a730e7b8cb07b1032e6b2bc0796a306a67cd6
Tamas <Tamas.Almos.Vami@cern.ch>
2021-04-23 04:48:46 +0200

    Modify config

---
+++ test/fromHSCP_internship2019/run_aod_data18.py
@@ -0,0 +1,106 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag.globaltag = "106X_dataRun2_v27"
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# /SingleMuon/Run2018*-12Nov2019_UL2018*/AOD
+# /SingleMuon/Run2018D-12Nov2019_UL2018-v4/AOD
+# root://cmsxrootd.fnal.gov//store/data/Run2018D/SingleMuon/AOD/12Nov2019_UL2018-v4/100000/63BF07E8-4AAA-DC4D-99DE-48B52FCB0C06.root
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+#         'root://cmsxrootd.fnal.gov//store/data/Run2017B/SingleMuon/AOD/17Nov2017-v1/70002/E6B4BA08-26D8-E711-AD9E-02163E0133DA.root' # aod 2017
+         'file:63BF07E8-4AAA-DC4D-99DE-48B52FCB0C06.root'
+    )
+)
+
+process.stage = cms.EDAnalyzer('ntuple'
+     , format_file       = cms.string('AOD')
+     , isdata            = cms.bool(True)
+     , year              = cms.untracked.int32(2018)
+     , primaryVertexColl  = cms.InputTag('offlinePrimaryVertices')  #->AOD
+     , isotracks             = cms.InputTag("isolatedTracks")
+     , tracks             = cms.InputTag("generalTracks")
+     , collectionHSCP     = cms.InputTag("HSCParticleProducer")
+     , isoHSCP0            = cms.InputTag("HSCPIsolation","R005")
+     , isoHSCP1            = cms.InputTag("HSCPIsolation","R01")
+     , isoHSCP2            = cms.InputTag("HSCPIsolation","R03")
+     , isoHSCP3            = cms.InputTag("HSCPIsolation","R05")
+     , dedx               = cms.InputTag("dedxHitInfo")
+     , MiniDedx           = cms.InputTag("isolatedTracks")
+     , dEdxHitInfoPrescale = cms.InputTag("dedxHitInfo","prescale")
+     , muons              = cms.InputTag("muons")
+     , muonTOF            = cms.InputTag("muons","combined")
+     , muonTDT            = cms.InputTag("muons","dt")
+     , muonTCSC           = cms.InputTag("muons","csc")
+     , printOut           = cms.untracked.int32(-1)
+     , GenPart            = cms.InputTag("genParticles")
+     , runOnGS            = cms.bool(False)
+     , stripSimLinks      = cms.InputTag("simSiStripDigis")
+     , lumiScalerTag      = cms.InputTag("scalersRawToDigi")
+     , doRecomputeMuTim   = cms.bool(False)
+     , cscSegments        = cms.InputTag("cscSegments")
+     , dt4DSegments       = cms.InputTag("dt4DSegments")
+     , ROUList            = cms.vstring(
+                                      'TrackerHitsTIBLowTof',  # hard-scatter (prompt) collection, module label g4simHits
+                                      'TrackerHitsTIBHighTof',
+                                      'TrackerHitsTIDLowTof',
+                                      'TrackerHitsTIDHighTof',
+                                      'TrackerHitsTOBLowTof',
+                                      'TrackerHitsTOBHighTof',
+                                      'TrackerHitsTECLowTof',
+                                      'TrackerHitsTECHighTof'
+                                      )
+)
+
+
+
+process.dump=cms.EDAnalyzer('EventContentAnalyzer')
+
+process.TFileService = cms.Service("TFileService",
+     fileName = cms.string('nt_data_aod.root')
+ )
+
+process.load("SUSYBSMAnalysis.HSCP.HSCParticleProducer_cff")
+
+process.load('HLTrigger.HLTfilters.hltHighLevel_cfi')
+process.HSCPTrigger = process.hltHighLevel.clone()
+process.HSCPTrigger.TriggerResultsTag = cms.InputTag( "TriggerResults", "", "HLT" )
+process.HSCPTrigger.andOr = cms.bool( True ) #OR
+process.HSCPTrigger.throw = cms.bool( False )
+process.HSCPTrigger.HLTPaths = [ #check triggers
+#          "HLT_PFMET110_PFMHT110_IDTight_v*",  # sus-19-009 (2016) on MET_MHT
+          "HLT_PFMET120_PFMHT120_IDTight_v*",  # sus-19-009 (2016-2018) 
+#          "HLT_PFMET170_NoiseCleaned_v*",      # sus-19-009 (2016) on MET
+#          "HLT_PFMET170_JetIdCleaned_v*",      # sus-19-009 (2016)
+#          "HLT_PFMET170_HBHECleaned_v*",       # sus-19-009 (2016)
+#          "HLT_PFMET170_NotCleaned_v*",        # sus-19-009 (2016)
+          "HLT_Mu50_v*",                       # fractionaly charged on SingleMu
+          "HLT_OldMu100_*",                     # fractionaly charged on SingleMu
+          "HLT_TkMu100_*",                      # fractionaly charged on SingleMu
+]
+
+process.p = cms.Path(process.HSCPTrigger + process.HSCParticleProducerSeq +process.stage)
+#process.p = cms.Path(process.dump)
+
+
+
+
+## MessageLogger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+# If you run over many samples and you save the log, remember to reduce
+# the size of the output by prescaling the report of the event number
+process.MessageLogger.cerr.FwkReport.reportEvery = 5000
+process.MessageLogger.cerr.default.limit = 10
