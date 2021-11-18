ad0d6d0df5c2ed482399e1108cbd080b60f933b0
Dylan Apparu <dapparu@live.com>
2021-11-18 09:58:53 +0100

    K&C extraction

---
+++ test/UsefulScripts/DeDxStudy/dEdxSkimmer_cfg.py
@@ -0,0 +1,61 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("DEDXUNCSKIM")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.load('Configuration.StandardSequences.Services_cff')
+
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+
+
+process.options   = cms.untracked.PSet(
+      wantSummary = cms.untracked.bool(True),
+      SkipEvent = cms.untracked.vstring('ProductNotFound'),
+)
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.source = cms.Source("PoolSource",
+   fileNames = cms.untracked.vstring(
+   ),
+   inputCommands = cms.untracked.vstring("keep *", "drop *_MEtoEDMConverter_*_*")
+)
+
+
+#process.GlobalTag.globaltag = GTAG
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, "106X_dataRun2_v28", '')
+
+
+#make the pool output
+process.Out = cms.OutputModule("PoolOutputModule",
+     outputCommands = cms.untracked.vstring(
+         "drop *",
+         "keep EventAux_*_*_*",
+         "keep LumiSummary_*_*_*",
+         "keep *_generalTracks_*_*",
+         "keep *_dedxHitInfo_*_*",
+         "keep *_muons_*_*",
+    ),
+    fileName = cms.untracked.string("dEdxSkim.root"),
+    SelectEvents = cms.untracked.PSet(
+       SelectEvents = cms.vstring('*')
+    ),
+)
+
+#schedule the sequence
+process.p = cms.Path()
+process.endPath1 = cms.EndPath(process.Out)
+process.schedule = cms.Schedule(process.p, process.endPath1)
+
+process.Out.fileName = cms.untracked.string('dEdxSkim.root')
+
+process.source.fileNames.extend(['root://cms-xrd-global.cern.ch/or', 'root://cms-xrd-global.cern.ch/directory', 'root://cms-xrd-global.cern.ch//cvmfs/cms.cern.ch/share/overrides/bin/das_client:', 'root://cms-xrd-global.cern.ch/line'])
+
+#import PhysicsTools.PythonAnalysis.LumiList as LumiList
+#process.source.lumisToProcess = LumiList.LumiList(filename = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt').getVLuminosityBlockRange()
