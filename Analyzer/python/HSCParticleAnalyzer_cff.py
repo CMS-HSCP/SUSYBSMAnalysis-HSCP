926ea72b3c0a5353deb10076257dbacb498cf404
Tamas <Tamas.Almos.Vami@cern.ch>
2022-03-10 21:11:41 +0100

    Comment out era modification for now

--- Analyzer/python/HSCParticleAnalyzer_cff.py
+++ Analyzer/python/HSCParticleAnalyzer_cff.py
@@ -4,14 +4,15 @@ import os
 from SUSYBSMAnalysis.Analyzer.HSCParticleAnalyzer_cfi import HSCParticleAnalyzer as analyzer 
 PATH_TO_DATA = "{}/src/SUSYBSMAnalysis/HSCP/data".format(os.getenv('CMSSW_BASE'))
 
-analyzer_2016 = SUSYBSMAnalysis.Analyzer.HSCParticleAnalyzer_cfi.HSCParticleAnalyzer.clone(
+''''
+analyzer_2016 = analyzer.clone(
     DeDxSF_0        = cms.untracked.double(1.00000), #=1 if data
     DeDxSF_1        = cms.untracked.double(1.0325), #2017 data
     DeDxK           = cms.untracked.double(2.30),
     DeDxC           = cms.untracked.double(3.17),
 )
 
-analyzer_2017 = SUSYBSMAnalysis.Analyzer.HSCParticleAnalyzer_cfi.HSCParticleAnalyzer.clone(
+analyzer_2017 = analyzer.clone(
     DeDxSF_0        = cms.untracked.double(1.00000), #=1 if data
     DeDxSF_1        = cms.untracked.double(1.0325), #2017 data
     DeDxK           = cms.untracked.double(2.30),
@@ -19,7 +20,7 @@ analyzer_2017 = SUSYBSMAnalysis.Analyzer.HSCParticleAnalyzer_cfi.HSCParticleAnal
     DeDxTemplate    = cms.untracked.string("{}/template_2017B.root".format(PATH_TO_DATA))
 )
 
-analyzer_2018 = SUSYBSMAnalysis.Analyzer.HSCParticleAnalyzer_cfi.HSCParticleAnalyzer.clone(
+analyzer_2018 = analyzer.Analyzer.HSCParticleAnalyzer_cfi.HSCParticleAnalyzer.clone(
     DeDxSF_0        = cms.untracked.double(1.00000), #=1 if data
     DeDxSF_1        = cms.untracked.double(1.0817), #2017 data
     DeDxK           = cms.untracked.double(2.27),
@@ -34,4 +35,5 @@ from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
 Run2_2016.toReplaceWith(analyzer, analyzer_2016)
 Run2_2017.toReplaceWith(analyzer, analyzer_2017)
 Run2_2018.toReplaceWith(analyzer, analyzer_2018)
+'''
 
