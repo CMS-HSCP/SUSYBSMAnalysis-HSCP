40a8aeab84e84c2e516e9121eddd4853cd204df5
Tamas <vami.tamas@wigner.mta.hu>
2022-09-27 23:29:31 +0200

    Add new (renamed) config files

---
+++ Analyzer/test/Tamas/scheduleOvernight.py
@@ -0,0 +1,10 @@
+import time, sys, os
+
+n = 0
+while(n<5):
+  n=n+1
+  os.system("python3 downloadCrabJobs.py 30p1")
+  os.system("python3 downloadCrabJobs.py 30p1")
+  os.system("python3 downloadCrabJobs.py 30p1")
+  time.sleep(7200)
+
---
+++ Analyzer/test/Tamas/scheduleOvernight_v2.py
@@ -0,0 +1,4 @@
+import time, sys, os
+
+time.sleep(10200)
+os.system("python3 haddCrabJobsMT.py 29p6")
