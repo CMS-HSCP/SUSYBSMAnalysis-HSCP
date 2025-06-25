fff7ce0b3e3ec9cb3daac292ba542f6bff53d9d3
coulon gael <gcoulon@sbgui2.in2p3.fr>
2025-06-25 11:25:06 +0200

    more debug

--- Analyzer/plugins/Analyzer.cc
+++ Analyzer/plugins/Analyzer.cc
@@ -4025,6 +4025,7 @@ void Analyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) 
     // for |eta|>1 (here |eta|<2.4), use track->pt() to generate the Gstrip template in order to have data at large eta 
     passedCutsArrayForGiTemplates[1] = ( (track->pt() > 20) && (track->pt() < 48) );
     // PAY ATTENTION : PU == NPV for the following loop !
+    if (debug_ == -12 ) LogPrint(MOD) << "Im here";
     if (passPreselection(passedCutsArrayForGiTemplates, false)) {
       if (doPostPreSplots_) {
         //check impact of no clustercleaning (in strip and in pix)
